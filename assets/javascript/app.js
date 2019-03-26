d76231a453493d217ce050dff95bc105ff4f611d
Robert Butler <robwbutler@gmail.com>
2019-03-26 18:32:53 -0400

    a bunch of stuff

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,65 @@
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    
+var animalList = [];
+
+function renderButtons() {
+
+  $("#buttons-view").empty();
+
+  for (var i = 0; i < animalList.length; i++) {
+
+    var a = $("<button>");
+    a.addClass("animal-btn");
+    a.attr("data-name", animalList[i]);
+    a.text(animalList[i]);
+    $("#buttons-view").append(a);
+  }
+}
+
+$("#search").on("click", function(event) {
+  event.preventDefault();
+  var animal = $(".form-control").val();
+
+  animalList.push(animal);
+
+  $(document).on("click", ".animal-btn", showAnimalGIFs);
+
+  renderButtons();
 })
+
+
+function showAnimalGIFs() {
+  var search = $(this).attr("data-name");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    search + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+console.log(search)
+console.log(queryURL)
+
+  $.ajax({
+        url: queryURL,
+        method: "GET",
+
+      }).then(function(response) {
+        var results = response.data
+        for (var i = 0; i < results.length; i++) {
+
+          var item = results[i];
+          var animalDiv = $("<div>");
+          var rate = item.rating.toUpperCase();
+
+          var p = $("<p>");
+          p.text("Rating: " + rate);
+
+          var animalImage = $("<img>");
+
+          animalImage.attr("src", item.images.fixed_height_still.url);
+          animalImage.addClass("pic");
+
+          animalDiv.append(animalImage);
+          animalDiv.append(p);
+
+          $("#gifs-appear-here").prepend(animalDiv);
+        }
+    })
+  
+}
+
